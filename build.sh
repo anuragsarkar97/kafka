0e3f1ed123d0bcd6af3840ca6a6a9a78545e612c
Anurag Sarkar <angsak6@gmail.com>
2025-09-28 09:21:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-apt-get update -y && apt-get install -y openjdk-17-jdk git curl unzip
- ./gradlew clean releaseTarGz -x signArchives
+# apt-get update -y && apt-get install -y openjdk-17-jdk git curl unzip
+#  ./gradlew clean releaseTarGz -x signArchives
+
+sleep infinity
